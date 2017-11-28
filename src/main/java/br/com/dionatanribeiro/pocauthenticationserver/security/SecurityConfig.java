d534e03499f392f89b063d56d8e4b9e3205104b4
Dionatan Ribeiro <dionatan.ribeiro@cwi.com.br>
2017-11-28 07:49:45 -0200

    remover arquivos desnecessarios

--- src/main/java/br/com/dionatanribeiro/pocauthenticationserver/security/SecurityConfig.java
+++ src/main/java/br/com/dionatanribeiro/pocauthenticationserver/security/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .headers().frameOptions().disable()
-            .and()
+                .and()
             .csrf().disable();
     }
 
